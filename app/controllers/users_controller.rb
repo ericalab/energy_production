1236fe6d423771d9acb4baf8454c620e86effcdb
erica <erika.yyma@gmail.com>
2020-10-14 13:37:14 +0900

    インポート機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
-  def new
+  def index
+    @users = User.all
+  end
+
+  def import
+    User.import(params[:fileupload][:file])
+    redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      user = find_by(id: row["id"]) || new
+      user.attributes = row.to_hash.slice(*updatable_attributes)
+      user.save
+    end
+  end
+
+  def self.updatable_attributes
+    ["id", "first_name", "last_name", "city", "num_of_people", "has_child"]
+  end
 end
